21f063c58428abbd2760ce2b49accef0cd702d5d
fmajer <filip.majer01@gmail.com>
2024-04-10 17:37:41 +0200

    zkousim neuronku

--- run.py
+++ run.py
@@ -4,6 +4,8 @@ from torch.utils.data import random_split, DataLoader
 from torchvision import transforms
 from methods import detect_edges, create_feature_vector
 from utilities import calculate_accuracy
+import torch
+import torch.nn as nn
 
 data_transform = transforms.Compose([
     transforms.ToTensor(),
@@ -12,31 +14,15 @@ data_transform = transforms.Compose([
 
 incision_dataset = IncisionDataset(xml_file='data/annotations.xml',
                                    image_dir='data/',
-                                   transform=None)
+                                   transform=data_transform)
 
 image_id = 0
 img, gray_img, thr_img, mask, n_stitches = incision_dataset.__getitem__(image_id)
 
-plt.imshow(gray_img, cmap='gray')
-plt.imshow(thr_img, cmap='gray')
-
-n_stitches_pred = detect_edges(gray_img)
+# n_stitches_pred = detect_edges(gray_img)
 # accuracy = calculate_accuracy(incision_dataset, detect_edges)
 # print(f"Accuracy: {accuracy * 100:.2f}%")
-
 # create_feature_vector(gray_img)
-
-
-
-
-
-
-
-
-
-
-
-# -------------------------------------------------USELESS (for now)----------------------------------------------------
 
 # Split dataset into training and validation
 train_percentage = 0.8
@@ -45,7 +31,8 @@ val_size = incision_dataset.__len__() - train_size
 train_dataset, val_dataset = random_split(incision_dataset, [train_size, val_size])
 
 # Create dataloaders
-batch_size = 1
-train_dataloader = DataLoader(train_dataset, batch_size=batch_size)
-val_dataloader = DataLoader(val_dataset, batch_size=batch_size)
+train_batch_size = 1
+val_batch_size = 1
+train_dataloader = DataLoader(train_dataset, batch_size=train_batch_size)
+val_dataloader = DataLoader(val_dataset, batch_size=val_batch_size)
 # print(next(iter(train_dataloader))[0].shape)
